f98ef7591746ae11260e271b3af0ccb8faaa8725
Natasha Rooney <nrooney@Natashas-MacBook-Air.local>
2015-02-24 17:26:30 +0100

    before switching back to old keys

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,9 @@
+var c = document.getElementById("aCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
+
 $( document ).ready(function() {
  
     
